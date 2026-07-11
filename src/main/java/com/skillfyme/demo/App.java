031ed1fb85479a13d78b720bc4e9b1762dadc9a4
pmaity1995 <prasantamaity549@gmail.com>
2026-07-11 17:12:57 +0530

    Fix Checkstyle package documentation and newline

--- src/main/java/com/skillfyme/demo/App.java
+++ src/main/java/com/skillfyme/demo/App.java
@@ -1,12 +1,31 @@
 package com.skillfyme.demo;
 
-public class App {
+/**
+ * Main application class for the Jenkins Pipeline as Code demo.
+ */
+public final class App {
 
+    /**
+     * Private constructor prevents creation of utility class instances.
+     */
+    private App() {
+    }
+
+    /**
+     * Returns the application message.
+     *
+     * @return application message
+     */
     public static String getMessage() {
         return "Jenkins Pipeline as Code is working!";
     }
 
-    public static void main(String[] args) {
+    /**
+     * Application entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
         System.out.println(getMessage());
     }
 }
---
+++ src/main/java/com/skillfyme/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the Jenkins Pipeline as Code demonstration application.
+ */
+package com.skillfyme.demo;
